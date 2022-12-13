08d3b4e24d65790f7ca6b922170743792bc8153a
Naofal <naofal@naofal.xyz>
2022-12-13 15:38:58 +0300

    Add Movie model

--- movie_hub/models.py
+++ movie_hub/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from djongo import models
+from djongo.models import (Model, CharField, TextField, IntegerField,
+                           DecimalField, DurationField)
+
 
 # Create your models here.
+class Movie(Model):
+    title = CharField(max_length=60)
+    year = IntegerField()
+    duration = DurationField()
